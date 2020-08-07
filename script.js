201227fa305212956178eb924361f4a42bb078f3
Djisu <pfleischer2002@yahoo.co.uk>
2020-08-07 14:08:25 +0100

    A commit for script.js

--- script.js
+++ script.js
@@ -6,9 +6,17 @@ const board = document.getElementById('board')
 
 let circleTurn
 
-cellElements.forEach(cell => {
-    cell.addEventListener('click', handleClick, { once: true })
-})
+startGame()
+
+function startGame(){
+    circleTurn = false
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true })
+    })
+    setBoardHoverClass()
+}
+
+
 
 function handleClick(e){
     const cell = e.target
